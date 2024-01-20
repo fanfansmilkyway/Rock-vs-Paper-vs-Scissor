0b05adf9942851966dae97d193ce1c0965e0e40a
fanfansmilkyway <fanfansmilkyway@qq.com>
2024-01-20 17:56:31 +0800

    BETA1.1(Statistic&icon)

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from tkinter import *
 import random
 import time
 
+import matplotlib.pyplot as plt
+
 GAMING = True
+time_start = time.time()
 
 # Initialize Game
 class Game:
     def __init__(self):
         tk = Tk()
         self.tk = tk
+        self.tk.title("Rock vs Paper vs Scissor(BETA1.1)")
         self.canvas = Canvas(tk, width=1200, height=850, background='grey')
+        self.canvas.title()
         self.canvas.pack()
 
 # Initialize three 'competitors'
@@ -149,7 +154,10 @@ papers_scoreboard.place(x=0, y=20)
 scissors_scoreboard = Label(tk, text="Scissor:20")
 scissors_scoreboard.place(x=0, y=40)
 
+winner_team = ""
+
 def update_scoreboard(rocks_score:int, papers_score:int, scissors_score:int):
+    global winner_team
     scores = [rocks_score, papers_score, scissors_score]
     scoreboards = [rocks_scoreboard, papers_scoreboard, scissors_scoreboard]
     for scoreboard_name in scoreboards:
@@ -166,6 +174,12 @@ def update_scoreboard(rocks_score:int, papers_score:int, scissors_score:int):
     except ValueError:
         pass
     else:
+        if scoreboards.index(winner) == 0:
+            winner_team = "Rock"
+        if scoreboards.index(winner) == 1:
+            winner_team = "Paper"
+        if scoreboards.index(winner) == 2:
+            winner_team = "Scissor"
         current_status = winner["text"]
         text, previous_score = current_status.split(":")
         final_text = text + ":" + "Winner!"
@@ -173,10 +187,14 @@ def update_scoreboard(rocks_score:int, papers_score:int, scissors_score:int):
         game_over()
 
 restart_button = None
+statistic_button = None
+time_game = 0
 
 def game_over():
-    time.sleep(2)
-    global GAMING, restart_button
+    global GAMING, restart_button, time_game, statistic_button
+    time_end = time.time()
+    time_game = time_end-time_start
+    time.sleep(1)
     GAMING = False
     rocks.clear()
     papers.clear()
@@ -184,15 +202,44 @@ def game_over():
     canvas.delete('all')
     restart_button = Button(tk, text='Restart', command=restart_game)
     restart_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+    statistic_button = Button(tk, text='Statistic', command=statistic)
+    statistic_button.place(relx=0.5, rely=0.54, anchor=CENTER) 
+    
+def statistic():
+    time_list = []
+    a = 0
+    time_increment = time_game / len(rock_number)
+    print(time_game)
+    for i in range(len(rock_number)):
+        a += time_increment
+        time_list.append(a)
+    print(len(rock_number))
+    plt.plot(time_list ,rock_number, 'r')
+    plt.plot(time_list, paper_number, 'b')
+    plt.plot(time_list, scissor_number, 'g')
+    plt.ylim(0, 60)
+    plt.title("Rock vs Paper vs Scissor({0} Wins)\nRed:Rock, Blue:Paper, Green:Scissor".format(winner_team))
+    plt.xlabel("Time(s)")
+    plt.ylabel("Survival Number")
+    plt.show()
 
 def restart_game():
-    global rocks, papers, scissors, GAMING, restart_button
+    global rocks, papers, scissors, GAMING, restart_button, statistic_button, rock_number, paper_number, scissor_number
+    time_start = time.time()
     restart_button.destroy()
+    statistic_button.destroy()
+    rock_number = []
+    paper_number = []
+    scissor_number = []
     rocks = [Rock(canvas, random.randint(50,canvas.winfo_reqwidth()-50), random.randint(50, canvas.winfo_reqheight()-50)) for i in range(20)]
     papers = [Paper(canvas, random.randint(50,canvas.winfo_reqwidth()-50), random.randint(50, canvas.winfo_reqheight()-50)) for i in range(20)]
     scissors = [Scissor(canvas, random.randint(50,canvas.winfo_reqwidth()-50), random.randint(50, canvas.winfo_reqheight()-50)) for i in range(20)]
     GAMING = True
-    
+
+rock_number = []
+paper_number = []
+scissor_number = []
+
 # Mainloop
 while True:
     if GAMING == True:
@@ -206,6 +253,9 @@ while True:
             scissor.draw()
             scissor.fight()
         # Update Scoreboards
+        rock_number.append(len(rocks))
+        paper_number.append(len(papers))
+        scissor_number.append(len(scissors))
         update_scoreboard(len(rocks), len(papers), len(scissors))
     tk.update_idletasks()
     tk.update()
